2552e5823b96bf4158bff3d0a21d194c1bb9848f
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2025-11-24 10:13:13 -0500

    Update Neovim config to get rid of warnings.

---
+++ config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        emmet_ls = {
+          filetypes = {
+            "html",
+            "typescriptreact",
+            "javascriptreact",
+            "css",
+            "sass",
+            "scss",
+            "less",
+            "javascript",
+            "typescript",
+            "markdown",
+            "ejs",
+            "twig", -- Add twig to the list of filetypes
+          },
+          init_options = {
+            html = {
+              options = {
+                ["bem.enabled"] = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
